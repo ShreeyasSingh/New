94276596d6fce924bb52eb30828196d3e7f85c9e
ShreeyasSingh <02ssingh17@gmail.com>
2025-09-27 17:26:31 +0530

    educated

---
+++ streamlit/widgets.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Streamlit text input")
+
+name=st.text_input("Enter your name:")
+
+if name:
+    st.write(f"Hello {name}!")
+else:
+    st.write("Please enter your name:")
+
+age=st.slider("Enter your age:",min_value=0, max_value=100, value=25)
+
+st.write(f"You are {age} years old")
+
+gender= st.selectbox("select your gender:", {"Male","Female","other"})
+st.write(f"You are {gender}")
+
+df=pd.DataFrame({
+    'name': [name],
+    'age':[age],
+    'gender': [gender]
+})
+
+st.write(df)
+uploaded_file=st.file_uploader("Upload a file:")##used to upload file from local machine
+if uploaded_file:
+    st.write(f"File uploaded:{uploaded_file.name}")
+else:
+    st.write("No file uploaded")
